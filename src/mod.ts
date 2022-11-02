cf7b23670658f5f13e51a91c1ed5f0b4232dd124
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-11-02 16:27:39 +0600

    chore: deno fmt

--- src/mod.ts
+++ src/mod.ts
@@ -1,11 +1,13 @@
 import { Command } from './types.ts';
 import ping from './commands/ping.ts';
 import help from './commands/help.ts';
+import anime from './commands/anime.ts';
 
 const commandMap = new Map<Command['data']['name'], Command>();
 
 commandMap
 	.set(ping.data.name, ping)
-	.set(help.data.name, help);
+	.set(help.data.name, help)
+	.set(anime.data.name, anime);
 
 export const commands = commandMap;
--- src/util.ts
+++ src/util.ts
@@ -27,3 +27,100 @@ export async function request(
 	}
 	return res;
 }
+
+type Options = {
+	method: 'POST';
+	headers: {
+		'Content-Type': 'application/json';
+		'Accept': 'application/json';
+	};
+	body: string;
+};
+
+/**
+ * Base function to make requests
+ * @private
+ * @export
+ * @param {string} url
+ * @param {Options} options
+ * @returns {*}
+ */
+export async function base_req_al(url: string, options: Options) {
+	return await fetch(url, options as Record<string, unknown>)
+		.then((response) => {
+			return response.json()
+				.then((json) => {
+					return response.ok ? json : Promise.reject(json);
+				});
+		})
+		.then((e) => e.data)
+		.catch((errors) => {
+			const error = errors[0] as Record<string, unknown>;
+			if (error.message === 'Not Found.') {
+				throw new Error(
+					`Not found. Got status code ${error.status}`,
+				);
+			} else {
+				throw new Error(
+					`Unknown error. Got status code ${error.status}. Reason: ${error.message}`,
+				);
+			}
+		});
+}
+
+/**
+ * Simplified version of request. Options and url is set, only provide the body
+ * @private
+ * @export
+ * @param {Record<string, unknown>} body
+ * @returns {*}
+ */
+export async function req_al(
+	{ query, variables }: {
+		query: string;
+		variables: Record<string, unknown>;
+	},
+) {
+	return await base_req_al('https://graphql.anilist.co', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json',
+		},
+		body: JSON.stringify({
+			query: query,
+			variables: variables,
+		}),
+	});
+}
+
+/**
+ * Shortens a string upto provided length
+ * @param {string} string the string to shorten
+ * @param {number} num length upto which to shorten
+ * @returns string
+ */
+export function shorten(string: string, num = 1000) {
+	if (typeof string !== 'string') return undefined;
+
+	if (string.length > num) {
+		return string.substring(0, num + 1) + '...';
+	} else {
+		return string;
+	}
+}
+/**
+ * Coverts the first character of the string to uppercase and the rest to lowercase
+ * @param {string} string the string to casify
+ * @returns string
+ */
+export function casify(string: string) {
+	if (typeof string == 'string') {
+		const space = string.split(/_/g);
+		return space.map((word) =>
+			word.charAt(0).toUpperCase() + word.substring(1).toLowerCase()
+		).join(' ');
+	} else {
+		return 'null';
+	}
+}
